cb48e5103835788235cebdb1026dcb54ca116169
LTeago <thiago_lopes7@outlook.com>
2025-04-02 14:26:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addValue = function(btn, value, next = true){
+  let field = $(btn).next();
+  if(!next){
+    field = $(btn).prev();
+  }
+  
+  let prevValue = parseInt($(field).val());
+  if(isNaN(prevValue)){
+    prevValue = 0;
+  }
+
+  let newValue = prevValue+value;
+  if(newValue < $(field).attr("min")){
+     newValue = $(field).attr("min");
+  }      
+  $(field).val(newValue);
+}
